8451fbb771ba80302c015394638215d53b8dc4ea
Kyle Oba <koba@mudphone.com>
2013-11-11 17:12:49 -1000

    Simple joke rotation, without using Joker class.

--- Simple/AAViewController.m
+++ Simple/AAViewController.m
@@ -11,12 +11,36 @@
 @interface AAViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *questionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak, nonatomic) IBOutlet UIButton *tellMeButton;
+@property (nonatomic, strong) NSArray *questions;
+@property (nonatomic, strong) NSArray *answers;
+@property (nonatomic, assign) NSUInteger currentIndex;
 @end
 
 @implementation AAViewController
 
+- (NSArray *)questions
+{
+    if (!_questions) {
+        _questions = @[@"Why did the chicken cross the road?",
+                       @"Why did the chicken cross the playground?"];
+    }
+    return _questions;
+}
+
+- (NSArray *)answers
+{
+    if (!_answers) {
+        _answers = @[@"To get to the other side.",
+                     @"To get to the other slide."];
+    }
+    return _answers;
+}
+
 - (void)showNextJoke
 {
+    self.questionLabel.text = [self.questions objectAtIndex:self.currentIndex];
+    self.answerLabel.text = [self.answers objectAtIndex:self.currentIndex];
     self.answerLabel.alpha = 0.0;
 }
 
@@ -29,7 +53,18 @@
 
 - (IBAction)tellMePressed:(UIButton *)sender
 {
-    self.answerLabel.alpha = 1.0;
+    if (self.answerLabel.alpha != 1.0) {
+        // Show punchline!
+        self.answerLabel.alpha = 1.0;
+        [self.tellMeButton setTitle:@"Next joke."
+                           forState:UIControlStateNormal];
+    } else {
+        // Load another joke.
+        self.currentIndex = (self.currentIndex + 1) % [self.questions count];
+        [self showNextJoke];
+        [self.tellMeButton setTitle:@"Tell me."
+                           forState:UIControlStateNormal];
+    }
 }
 
 @end
